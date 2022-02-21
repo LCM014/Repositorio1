50117ac7a721c4c93be3d45e62464acc47a33278
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-02-21 19:28:25 +0000

    Att

--- Mensagem.java
+++ Mensagem.java
@@ -2,12 +2,11 @@ public class Mensagem
 {
      public static void main(String[] args) 
     {
+        int idade=23;
+        char sangue='O';
+        System.out.println("Lucas tem "+idade +" anos e sangue "+sangue);
         System.out.println("Fatec Pompeia");
 
-
-
-
-        
     }
 
 }
