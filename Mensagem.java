cd0dd6f218ff4df2fc0265623043dab27fec3af2
Lucas Carvalho Medeiros <lucas.medeiros5@fatec.sp.gov.br>
2022-02-21 19:55:12 +0000

    att

--- Mensagem.java
+++ Mensagem.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class Mensagem
-{
+{   
+    
      public static void main(String[] args) 
     {
-        int idade=23;
-        char sangue='O';
-        System.out.println("Lucas tem "+idade +" anos e sangue "+sangue);
+        int idade = 23;
+        char sangue ='O';
+        double media = 7.55;
+
         System.out.println("Fatec Pompeia");
+        System.out.println("Lucas tem "+idade +" anos e sangue -"+sangue);
+        
+        System.out.printf("A media de notas = %.2f\n",media);
+
+        int num;
+        System.out.println("Digite sua idade:");
+        Scanner entrada = new Scanner(System.in);
+        num = entrada.nextInt();
+
 
     }
 
